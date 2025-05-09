ddea2e5d363474efb45035d7320b99c9a04db199
不如 <69700008+xlzs001@users.noreply.github.com>
2025-05-09 11:08:27 +0800

    Update revcat2099.js

--- revcat2099.js
+++ revcat2099.js
@@ -1,24 +1,29 @@
-var response = JSON.parse($response.body);
+// Pillow Enhanced Subscription Unlock Script
 
-// 修改订阅数据
-if (response.subscriber && response.subscriber.subscriptions) {
-    for (var subscription in response.subscriber.subscriptions) {
-        if (response.subscriber.subscriptions.hasOwnProperty(subscription)) {
-            response.subscriber.subscriptions[subscription].expires_date = '2099-12-31T23:59:59Z';
-            response.subscriber.subscriptions[subscription].purchase_date = '2025-01-01T00:00:00Z';
+// 读取原始响应数据
+var objc = JSON.parse($response.body);
+
+// 确保原始数据结构存在
+if (objc.subscriber && objc.subscriber.subscriptions) {
+    // 遍历所有订阅项目
+    for (var subscription in objc.subscriber.subscriptions) {
+        if (objc.subscriber.subscriptions.hasOwnProperty(subscription)) {
+            // 修改到期日期和购买日期
+            objc.subscriber.subscriptions[subscription].expires_date = '2099-12-31T23:59:59Z';
+            objc.subscriber.subscriptions[subscription].purchase_date = '2025-01-01T00:00:00Z';
         }
     }
-}
 
-// 修改权限信息
-if (response.subscriber.entitlements) {
-    for (var entitlement in response.subscriber.entitlements) {
-        if (response.subscriber.entitlements.hasOwnProperty(entitlement)) {
-            response.subscriber.entitlements[entitlement].expires_date = '2099-12-31T23:59:59Z';
-            response.subscriber.entitlements[entitlement].purchase_date = '2025-01-01T00:00:00Z';
+    // 确保权限信息也被同步修改
+    if (objc.subscriber.entitlements) {
+        for (var entitlement in objc.subscriber.entitlements) {
+            if (objc.subscriber.entitlements.hasOwnProperty(entitlement)) {
+                objc.subscriber.entitlements[entitlement].expires_date = '2099-12-31T23:59:59Z';
+                objc.subscriber.entitlements[entitlement].purchase_date = '2025-01-01T00:00:00Z';
+            }
         }
     }
 }
 
 // 返回修改后的数据
-$done({body: JSON.stringify(response)});
+$done({body: JSON.stringify(objc)});
